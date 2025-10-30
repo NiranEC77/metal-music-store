132116c5b504c9c7b92c8776ee77564235ef5c2f
niranec77 <niranec@gmail.com>
2025-10-30 16:41:59 -0400

    added tfvars and gitignore

--- antrea-nsx-dfw/generated/main.tf
+++ antrea-nsx-dfw/generated/main.tf
@@ -1,8 +1,3 @@
-variable "nsxt_host" { type = string }
-variable "nsxt_username" { type = string }
-variable "nsxt_password" { type = string, sensitive = true }
-variable "allow_unverified_ssl" { type = bool, default = true }
-
 terraform {
   required_providers {
     nsxt = {
