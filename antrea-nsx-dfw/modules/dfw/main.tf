d3894d5853ca45ece2057996281a6d1510abdbf2
niranec77 <niranec@gmail.com>
2025-10-30 17:35:03 -0400

    so many errors in apply

--- antrea-nsx-dfw/modules/dfw/main.tf
+++ antrea-nsx-dfw/modules/dfw/main.tf
@@ -60,53 +60,163 @@ resource "nsxt_policy_service" "svc_tcp_5432" {
 resource "nsxt_policy_group" "store_service" {
   display_name = "store-service"
   criteria {
-    condition { member_type = "SegmentPort" key = "Tag" operator = "EQUALS" value = "service-name:store" }
-    condition { member_type = "SegmentPort" key = "Tag" operator = "EQUALS" value = "app-name:music-store" }
-    condition { member_type = "SegmentPort" key = "Tag" operator = "EQUALS" value = "env:prod" }
-    condition { member_type = "SegmentPort" key = "Tag" operator = "EQUALS" value = "namespace:name:music-store" }
+    condition {
+      member_type = "SegmentPort"
+      key         = "Tag"
+      operator    = "EQUALS"
+      value       = "service-name:store"
+    }
+    condition {
+      member_type = "SegmentPort"
+      key         = "Tag"
+      operator    = "EQUALS"
+      value       = "app-name:music-store"
+    }
+    condition {
+      member_type = "SegmentPort"
+      key         = "Tag"
+      operator    = "EQUALS"
+      value       = "env:prod"
+    }
+    condition {
+      member_type = "SegmentPort"
+      key         = "Tag"
+      operator    = "EQUALS"
+      value       = "namespace:name:music-store"
+    }
   }
 }
 resource "nsxt_policy_group" "cart_service" {
   display_name = "cart-service"
   criteria {
-    condition { member_type = "SegmentPort" key = "Tag" operator = "EQUALS" value = "service-name:cart" }
-    condition { member_type = "SegmentPort" key = "Tag" operator = "EQUALS" value = "app-name:music-store" }
-    condition { member_type = "SegmentPort" key = "Tag" operator = "EQUALS" value = "env:prod" }
-    condition { member_type = "SegmentPort" key = "Tag" operator = "EQUALS" value = "namespace:name:music-store" }
+    condition {
+      member_type = "SegmentPort"
+      key         = "Tag"
+      operator    = "EQUALS"
+      value       = "service-name:cart"
+    }
+    condition {
+      member_type = "SegmentPort"
+      key         = "Tag"
+      operator    = "EQUALS"
+      value       = "app-name:music-store"
+    }
+    condition {
+      member_type = "SegmentPort"
+      key         = "Tag"
+      operator    = "EQUALS"
+      value       = "env:prod"
+    }
+    condition {
+      member_type = "SegmentPort"
+      key         = "Tag"
+      operator    = "EQUALS"
+      value       = "namespace:name:music-store"
+    }
   }
 }
 resource "nsxt_policy_group" "order_service" {
   display_name = "order-service"
   criteria {
-    condition { member_type = "SegmentPort" key = "Tag" operator = "EQUALS" value = "service-name:order" }
-    condition { member_type = "SegmentPort" key = "Tag" operator = "EQUALS" value = "app-name:music-store" }
-    condition { member_type = "SegmentPort" key = "Tag" operator = "EQUALS" value = "env:prod" }
-    condition { member_type = "SegmentPort" key = "Tag" operator = "EQUALS" value = "namespace:name:music-store" }
+    condition {
+      member_type = "SegmentPort"
+      key         = "Tag"
+      operator    = "EQUALS"
+      value       = "service-name:order"
+    }
+    condition {
+      member_type = "SegmentPort"
+      key         = "Tag"
+      operator    = "EQUALS"
+      value       = "app-name:music-store"
+    }
+    condition {
+      member_type = "SegmentPort"
+      key         = "Tag"
+      operator    = "EQUALS"
+      value       = "env:prod"
+    }
+    condition {
+      member_type = "SegmentPort"
+      key         = "Tag"
+      operator    = "EQUALS"
+      value       = "namespace:name:music-store"
+    }
   }
 }
 resource "nsxt_policy_group" "users_service" {
   display_name = "users-service"
   criteria {
-    condition { member_type = "SegmentPort" key = "Tag" operator = "EQUALS" value = "service-name:users" }
-    condition { member_type = "SegmentPort" key = "Tag" operator = "EQUALS" value = "app-name:music-store" }
-    condition { member_type = "SegmentPort" key = "Tag" operator = "EQUALS" value = "env:prod" }
-    condition { member_type = "SegmentPort" key = "Tag" operator = "EQUALS" value = "namespace:name:music-store" }
+    condition {
+      member_type = "SegmentPort"
+      key         = "Tag"
+      operator    = "EQUALS"
+      value       = "service-name:users"
+    }
+    condition {
+      member_type = "SegmentPort"
+      key         = "Tag"
+      operator    = "EQUALS"
+      value       = "app-name:music-store"
+    }
+    condition {
+      member_type = "SegmentPort"
+      key         = "Tag"
+      operator    = "EQUALS"
+      value       = "env:prod"
+    }
+    condition {
+      member_type = "SegmentPort"
+      key         = "Tag"
+      operator    = "EQUALS"
+      value       = "namespace:name:music-store"
+    }
   }
 }
 resource "nsxt_policy_group" "database_service" {
   display_name = "database-service"
   criteria {
-    condition { member_type = "SegmentPort" key = "Tag" operator = "EQUALS" value = "service-name:database" }
-    condition { member_type = "SegmentPort" key = "Tag" operator = "EQUALS" value = "app-name:music-store" }
-    condition { member_type = "SegmentPort" key = "Tag" operator = "EQUALS" value = "env:prod" }
-    condition { member_type = "SegmentPort" key = "Tag" operator = "EQUALS" value = "namespace:name:music-store" }
+    condition {
+      member_type = "SegmentPort"
+      key         = "Tag"
+      operator    = "EQUALS"
+      value       = "service-name:database"
+    }
+    condition {
+      member_type = "SegmentPort"
+      key         = "Tag"
+      operator    = "EQUALS"
+      value       = "app-name:music-store"
+    }
+    condition {
+      member_type = "SegmentPort"
+      key         = "Tag"
+      operator    = "EQUALS"
+      value       = "env:prod"
+    }
+    condition {
+      member_type = "SegmentPort"
+      key         = "Tag"
+      operator    = "EQUALS"
+      value       = "namespace:name:music-store"
+    }
   }
 }
 resource "nsxt_policy_group" "music_store_app" {
   display_name = "music-store"
   criteria {
-    condition { member_type = "SegmentPort" key = "Tag" operator = "EQUALS" value = "app-name:music-store" }
-    condition { member_type = "SegmentPort" key = "Tag" operator = "EQUALS" value = "env:prod" }
+    condition {
+      member_type = "SegmentPort"
+      key         = "Tag"
+      operator    = "EQUALS"
+      value       = "app-name:music-store"
+    }
+    condition {
+      member_type = "SegmentPort"
+      key         = "Tag"
+      operator    = "EQUALS"
+      value       = "env:prod"
+    }
   }
 }
 resource "nsxt_policy_group" "frontend" {
