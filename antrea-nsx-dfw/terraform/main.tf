b5ff0947daba96de5d1d3d9b1a6ec13c38e4f8a1
niranec77 <niranec@gmail.com>
2025-10-30 16:55:33 -0400

    fixed files lsyout

--- antrea-nsx-dfw/terraform/main.tf
+++ antrea-nsx-dfw/terraform/main.tf
@@ -1,5 +1,10 @@
 module "dfw" {
-  source = "../generated"
+  source = "../modules/dfw"
+
+  nsxt_host             = var.nsxt_host
+  nsxt_username         = var.nsxt_username
+  nsxt_password         = var.nsxt_password
+  allow_unverified_ssl  = var.allow_unverified_ssl
 }
 
 
