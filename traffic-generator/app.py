7abcbaad825540cf1a397c5034b4bd1b47342e10
niranec77 <niranec@gmail.com>
2026-01-23 01:35:06 -0500

    Fix Chrome DevToolsActivePort error: add shm volume and additional Chrome options

--- traffic-generator/app.py
+++ traffic-generator/app.py
@@ -56,10 +56,25 @@ def create_driver(headless=True):
     chrome_options = Options()
     if headless:
         chrome_options.add_argument('--headless')
+    
+    # Essential options for containerized environments
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
     chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--disable-software-rasterizer')
+    chrome_options.add_argument('--disable-extensions')
+    chrome_options.add_argument('--disable-setuid-sandbox')
     chrome_options.add_argument('--window-size=1920,1080')
+    chrome_options.add_argument('--remote-debugging-port=9222')
+    chrome_options.add_argument('--disable-dev-tools')
+    chrome_options.add_argument('--no-first-run')
+    chrome_options.add_argument('--no-default-browser-check')
+    chrome_options.add_argument('--single-process')
+    chrome_options.add_argument('--ignore-certificate-errors')
+    
+    # Additional stability options
+    chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
+    chrome_options.add_experimental_option('useAutomationExtension', False)
     
     driver = webdriver.Chrome(options=chrome_options)
     driver.set_page_load_timeout(30)
